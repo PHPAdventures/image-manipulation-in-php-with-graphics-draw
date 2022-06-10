b17ac64bd265a5e0aaf80c7148f047a39c2ad7ef
Warren <hello@warren.codes>
2022-06-09 18:51:36 -0600

    Add buttons example.

---
+++ 01-gd-buttons.php
@@ -0,0 +1,61 @@
+<?php
+    // Send back an image if we're receiving text for the button.
+    if ( isset( $_GET['text'] ) ) {
+        define('ASSET_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'assets');
+        
+        // Let the browser know this is an image, NOT a web page!
+        header('Content-type: image/png');
+
+        $string = $_GET['text'];
+
+        $image = imageCreateFromPNG(ASSET_DIR . DIRECTORY_SEPARATOR . 'button.png');
+
+        // Font colour: white.
+        $fontColour = imageColorAllocate(
+            $image, // The image we want to get a colour ready for.
+            255,    // RED
+            255,    // GREEN
+            255     // BLUE
+        );
+
+        $fontSize = 12;
+
+        // Add the string to our image.
+        imageString(
+            $image,     // Which image do you want to print on?
+            $fontSize,  // Size of font.
+            2,          // X co-ordinate on the image (from top-left.)
+            14,         // Y co-ordinate on the image (from top-left.)
+            $string,    // Text to print on image.
+            $fontColour // Colour of font.
+        );
+
+        // Sends the image.
+        imagePNG($image);
+
+        // Good practice to get rid of it.. (PHP is usually good at this on its own!)
+        imageDestroy($image);
+
+        exit;
+    }
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>01 GD Buttons</title>
+</head>
+<body>
+    <h1>01 GD Buttons</h1>
+    <p>
+        <a href="https://phpadventures.com">
+            <img src="/01-gd-buttons.php?text=PHP+Adventures" alt="PHP Adventures">
+        </a>
+        <a href="https://www.bioware.com/">
+            <img src="/01-gd-buttons.php?text=BioWare" alt="BioWare">
+        </a>
+    </p>
+    
+</body>
+</html>
